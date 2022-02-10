3c5e7ddd3dd9d85ac338b73b523974f09ad48a65
Jenna Qian <jiayin1996@gmail.com>
2022-02-10 18:16:30 -0500

    create snacks table

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -3,4 +3,13 @@ CREATE DATABASE snack_a_log;
 
 \c snack_a_log; 
 
+CREATE TABLE snacks (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    image TEXT,
+    fiber NUMERIC DEFAULT 0, 
+    protein NUMERIC DEFAULT 0,
+    added_sugar NUMERIC DEFAULT 0,
+    is_healthy BOOLEAN
+);
 
