edab099258d40ab1c7e062d07deb395383ef008d
Jenna Qian <jiayin1996@gmail.com>
2022-02-11 11:17:41 -0500

    app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,24 @@
 // DEPENDENCIES
-
 const express = require("express");
+const cors = require("cors")
 
 // CONFIGURATION
 const app = express();
 
 // MIDDLEWARE
+app.use(cors())
+app.use(express.json())
+require("dotenv").config()
 
 // ROUTES
+app.get("/", (req, res)=> {
+    res.send("Welcome to Snack World!")
+})
+
+//404 PAGE
+app.get("*", (req, res)=> {
+    res.status(404).send("Page not found!")
+})
 
 // EXPORT
 module.exports = app;
