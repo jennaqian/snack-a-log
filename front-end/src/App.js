1f8e56830fbaafeebdb9610788293893e07ee316
Jenna Qian <jiayin1996@gmail.com>
2022-02-11 15:59:36 -0500

    Add Home and Navbar Component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,15 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./Components/Home.js"
+import Navbar from "./Components/Navbar.js"
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, world!</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
   );
 }
 
